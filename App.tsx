60f486b2e95cb145ed644c02b77426362f4720c7
Glauber Brack <glauber@brack.com.br>
2020-07-06 22:54:35 -0300

    first one

--- App.tsx
+++ App.tsx
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { AppLoading } from "expo";
+import { useFonts } from "@use-expo/font";
+
+import api from "./src/services/api";
 
 export default function App() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    api.get("/").then(({ data }) => setData(JSON.stringify(data[0])));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>{data}</Text>
     </View>
   );
 }
@@ -14,8 +22,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontFamily: "gilroy-semibold",
   },
 });
