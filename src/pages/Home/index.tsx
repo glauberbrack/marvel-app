49b22765744183d14dbc9dcc92001e764a6d8edd
Glauber Brack <glauber@brack.com.br>
2020-07-06 23:13:32 -0300

    section and card components

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import Constants from "expo-constants";
+import { LinearGradient } from "expo-linear-gradient";
 
 import Menu from "../../assets/icons/menu.svg";
 import Logo from "../../assets/icons/logo.svg";
 import Search from "../../assets/icons/search.svg";
+
+import Hero from "../../assets/icons/hero.svg";
+import Villain from "../../assets/icons/villain.svg";
+import Antihero from "../../assets/icons/antihero.svg";
+import Alien from "../../assets/icons/alien.svg";
+import Human from "../../assets/icons/human.svg";
 
 const Home = () => {
   return (
@@ -13,6 +20,29 @@ const Home = () => {
         <Menu />
         <Logo />
         <Search />
+      </View>
+
+      <View style={styles.head}>
+        <Text style={styles.title}>Bem vindo ao Marvel Heroes</Text>
+        <Text style={styles.subTitle}>Escolha o seu personagem</Text>
+      </View>
+
+      <View style={styles.categories}>
+        <LinearGradient colors={["#005BEA", "#00C6FB"]} style={styles.circle}>
+          <Hero color="#FFF"/>
+        </LinearGradient>
+        <LinearGradient colors={["#ED1D24", "#ED1F69"]} style={styles.circle}>
+          <Villain />
+        </LinearGradient>
+        <LinearGradient colors={["#B224EF", "#7579FF"]} style={styles.circle}>
+          <Antihero />
+        </LinearGradient>
+        <LinearGradient colors={["#0BA360", "#3CBA92"]} style={styles.circle}>
+          <Alien />
+        </LinearGradient>
+        <LinearGradient colors={["#FF7EB3", "#FF758C"]} style={styles.circle}>
+          <Human />
+        </LinearGradient>
       </View>
     </View>
   );
@@ -29,5 +59,33 @@ const styles = StyleSheet.create({
   bar: {
     flexDirection: "row",
     justifyContent: "space-between",
+  },
+  head: {
+    marginTop: 20,
+  },
+  title: {
+    fontFamily: "gilroy-semibold",
+    fontSize: 14,
+    color: "#B7B7C8",
+  },
+  subTitle: {
+    fontFamily: "gilroy-heavy",
+    fontSize: 32,
+    color: "#313140",
+  },
+  categories: {
+    marginTop: 20,
+    marginBottom: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  circle: {
+    width: 56,
+    height: 56,
+    borderRadius: 50,
+    alignContent: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#B7B7C8",
   },
 });
